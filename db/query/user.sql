1d3a7bc221be2c3072a64e87d31631b5da4cf070
Jimmy <dm.a.uvarov@gmail.com>
2022-08-11 13:12:54 -0400

    generate read queries

--- db/query/user.sql
+++ db/query/user.sql
@@ -5,3 +5,13 @@ INSERT INTO users (
   $1
 )
 RETURNING *;
+
+-- name: GetUser :one
+SELECT * FROM users
+WHERE id = $1 LIMIT 1;
+
+-- name: ListUsers :many
+SELECT * FROM users
+ORDER BY id
+LIMIT $1
+OFFSET $2;
--- db/sqlc/user.sql.go
+++ db/sqlc/user.sql.go
@@ -24,3 +24,50 @@ func (q *Queries) CreateUser(ctx context.Context, username string) (User, error)
 	err := row.Scan(&i.ID, &i.Username)
 	return i, err
 }
+
+const getUser = `-- name: GetUser :one
+SELECT id, username FROM users
+WHERE id = $1 LIMIT 1
+`
+
+func (q *Queries) GetUser(ctx context.Context, id int32) (User, error) {
+	row := q.db.QueryRowContext(ctx, getUser, id)
+	var i User
+	err := row.Scan(&i.ID, &i.Username)
+	return i, err
+}
+
+const listUsers = `-- name: ListUsers :many
+SELECT id, username FROM users
+ORDER BY id
+LIMIT $1
+OFFSET $2
+`
+
+type ListUsersParams struct {
+	Limit  int32 `json:"limit"`
+	Offset int32 `json:"offset"`
+}
+
+func (q *Queries) ListUsers(ctx context.Context, arg ListUsersParams) ([]User, error) {
+	rows, err := q.db.QueryContext(ctx, listUsers, arg.Limit, arg.Offset)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+	var items []User
+	for rows.Next() {
+		var i User
+		if err := rows.Scan(&i.ID, &i.Username); err != nil {
+			return nil, err
+		}
+		items = append(items, i)
+	}
+	if err := rows.Close(); err != nil {
+		return nil, err
+	}
+	if err := rows.Err(); err != nil {
+		return nil, err
+	}
+	return items, nil
+}
